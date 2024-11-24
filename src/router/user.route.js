5d9d95f1ffd1da62163cb8dd6dbdb8c50505d4df
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-24 23:02:53 +0600

    add log out

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
-const {register,singin} = require('../controller/user.auth.controller');
+const {register,singin, logout} = require('../controller/user.auth.controller');
 
 const userRouter = express.Router();
 
 userRouter.post('/register', register);
 userRouter.post('/singin', singin);
+userRouter.post('/logout', logout);
 
 module.exports = userRouter
