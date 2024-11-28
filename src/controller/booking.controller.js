b35ecfa44f24bd4a630ebadbcb6d106c25e21fc6
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-28 23:36:40 +0600

    bookin

---
+++ src/controller/booking.controller.js
@@ -0,0 +1,32 @@
+const { ReactReduxContext } = require("react-redux");
+const Booking = require("../model/booking.model");
+
+const addBooking = async (req, res) => {
+  try {
+    const { userId, propertyId } = req.body;
+
+    const allReadyExisting = await Booking.findOne({ propertyId });
+    if (allReadyExisting) {
+      return res.status(400).json({ message: "Booking already exist" });
+    }
+
+    const booking = await Booking.create({
+      userId,
+      propertyId,
+    });
+    res
+      .status(201)
+      .json({
+        message: "Add success Full",
+        success: true,
+        error: false,
+        booking,
+      });
+  } catch (error) {
+    res
+      .status(404)
+      .json({ message: error.message || error, success: false, error: true });
+  }
+};
+
+module.exports = { addBooking };
