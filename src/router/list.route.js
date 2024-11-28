2cf019f5b6da37bee1b9d9eac420eed942c98af3
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-28 23:44:52 +0600

    list router

--- src/router/list.route.js
+++ src/router/list.route.js
@@ -1,11 +1,14 @@
 
 const express = require('express');
-const { upload, addProperty  } = require('../controller/list.controller');
+const { addProperty , categoryProperty, singleProperty } = require('../controller/list.controller');
+const upload = require('../utils/multer');
 
 
 const listRouter = express.Router();
 
-listRouter.post('/add-property' , upload.array('photoList') , addProperty);
+listRouter.post('/add-property' , addProperty);
+listRouter.get('/category', categoryProperty)
+listRouter.get('/single-Property/:id', singleProperty)
 
 
 module.exports = listRouter;  //export the router to use in other files.  //export
