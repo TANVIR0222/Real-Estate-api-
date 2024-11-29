fb22d48cd374bc8bf4c946c1feb2ac4245ba4c0d
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-29 20:59:59 +0600

    add Booking

--- src/controller/booking.controller.js
+++ src/controller/booking.controller.js
@@ -1,8 +1,21 @@
 const Booking = require("../model/booking.model");
+const Listing = require("../model/list.model");
 
 const addBooking = async (req, res) => {
   try {
-    const { userId, propertyId } = req.body;
+    const {
+      userId,
+      propertyId,
+      category,
+      type,
+      city,
+      Province,
+      country,
+      listingPhotoPath,
+      title,
+      descriptions,
+      price,
+    } = req.body;
 
     const allReadyExisting = await Booking.findOne({ propertyId });
     if (allReadyExisting) {
@@ -12,6 +25,15 @@ const addBooking = async (req, res) => {
     const booking = await Booking.create({
       userId,
       propertyId,
+      category,
+      type,
+      city,
+      Province,
+      country,
+      listingPhotoPath,
+      title,
+      descriptions,
+      price,
     });
     res.status(201).json({
       message: "Add success Full",
@@ -26,4 +48,27 @@ const addBooking = async (req, res) => {
   }
 };
 
-module.exports = { addBooking };
+const fovoriteBooking = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const allReadyExisting = await Booking.find({ userId: id });
+    res.status(201).json(allReadyExisting);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ message: error.message || error, success: false, error: true });
+  }
+};
+const fovoritBooking = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const allReadyExisting = await Listing.find({ id });
+    res.status(201).json(allReadyExisting);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ message: error.message || error, success: false, error: true });
+  }
+};
+
+module.exports = { addBooking, fovoriteBooking };
